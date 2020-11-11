8eefaa79340c84c8ae01887f39bc87644bd3f66e
Alekslov <Alekslov@users.noreply.github.com>
2020-11-11 13:01:14 +0200

    main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+// /sveikieji skaiciai/ 
+console.log(8784655);
+// du skaiciai vs desimtainis
+console.log(3, 14);
+console.log(3.14);
+
+
+// tekstas
+console.log("labas rytas");
+console.log('labas rytas');
